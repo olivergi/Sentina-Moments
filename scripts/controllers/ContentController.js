75718768a0d6e64d5fd8e1f750f7811e32f9630b
Arttu Tiihonen <arttu.tiihonen@gmail.com>
2016-11-11 09:27:32 +0200

    Audio remembering when changing view is added and working. Changed the routing library to angular-ui-router.

--- scripts/controllers/ContentController.js
+++ scripts/controllers/ContentController.js
@@ -1,15 +1,34 @@
 'use strict';
 
-var app = angular.module('SentinaMoments', ['ngRoute', 'ngAudio']);
+var app = angular.module('SentinaMoments', ['ui.router', 'ngAudio']);
 	// Content routing for the application
-	app.config(function($routeProvider){
-		    $routeProvider
-		    .when("/", {
-		        templateUrl : "../../views/player.html"
-		    })
-		    .when("/menu", {
-		        templateUrl : "../../views/menu.html"
-		    });
+	app.config(function($urlRouterProvider, $stateProvider){
+		    $stateProvider
+
+		    .state("player", {
+                url: "/",
+                templateUrl: "../../views/player.html",
+                controller: function($scope, ngAudio, songRemember) {
+                    $scope.currentSong = "https://archive.org/download/InceptionSoundtrackHD12TimeHansZimmer/Inception%20Soundtrack%20HD%20-%20%2312%20Time%20%28Hans%20Zimmer%29.mp3";
+                    
+                    if (songRemember[$scope.currentSong]) {
+                        $scope.audio = songRemember[$scope.currentSong];
+                    } else {
+                        $scope.audio = ngAudio.load($scope.currentSong);
+                        songRemember[$scope.currentSong] = $scope.audio;
+
+                    }
+                }
+            })
+
+
+	        .state('menu', {
+	            url: "/menu",
+	            templateUrl: "../../views/menu.html",
+	        })
+
+
+        	$urlRouterProvider.otherwise('/');
 	});
 
 	app.value("songRemember",{})
