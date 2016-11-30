4a4ca0d3ae4bfce77e07578b24d1c116b81c6234
Arttu Tiihonen <arttu.tiihonen@gmail.com>
2016-11-30 10:30:12 +0200

    Restructured the files, ContentController renamed to MainController, Services and a factory added. Audio playing from the playlist working. Problems currently with getting more than 10 nonces from the backend.

---
+++ scripts/controllers/MainController.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var app = angular.module('SentinaMoments', ['ui.router', 'ngAudio']);
+// Content routing for the application
+app.config(function($urlRouterProvider, $stateProvider){
+	    $stateProvider
+
+	    .state("player", {
+            url: "/",
+            templateUrl: "../../views/player.html",
+            controller: function($scope, ngAudio, songRemember,$log, VariableFactory) {
+            	var url = VariableFactory.audios[VariableFactory.currentSong];
+
+                if (songRemember[url]) {
+                	// If the current song is remembered, load the file on the player.
+                    $scope.audio = songRemember[url];
+                } else {
+                	// Initial audio load
+                    //$scope.audio = ngAudio.load(url);
+                    //songRemember[url] = $scope.audio;
+
+                }
+            }
+        })
+
+        .state('menu', {
+            url: "/menu",
+            templateUrl: "../../views/menu.html",
+        })
+
+        .state('search', {
+        	url: "/search",
+            templateUrl: "../../views/search.html",
+        })
+
+
+    	$urlRouterProvider.otherwise('/');
+});
+
+// Value for remembering the audio when in other views
+app.value("songRemember",{})
+	// The main controller of the application
+    .controller('MainController', function($scope, ngAudio, $log, $state, $http, RequestService, VariableFactory) {
+	    // temporary setup for authorizing the user
+	    // TODO: Authorization through login page
+	    RequestService.formPost("auth/page/hashdb/login");
+	    // Get the user object
+	    RequestService.getUser();
+
+	  	// Get today's playlist
+		RequestService.getTodaysRecipe();
+
+        // Have a listener when the RequestService calls for loading a playlist with a recipeID
+		$scope.$on('startPlaylist', function(event) {
+	        $scope.audio = ngAudio.load(VariableFactory.audios[VariableFactory.currentSong]);
+	    })
+
+	    //Listener for getting the nonces once the user object is available
+		$scope.$on('getNonces', function(event) {
+		  	RequestService.getNonces()
+	    })
+
+        $scope.nextTrack = function() {
+        	$log.info("current audio: ",$scope.audio);    
+
+        	if (VariableFactory.currentSong >= VariableFactory.audios.length-1){
+        		// start the playlist again
+        		$log.info("playlist restart");
+        		VariableFactory.currentSong = 0;
+        		$scope.audio = ngAudio.load(VariableFactory.audios[VariableFactory.currentSong]);
+        	} else {
+        		// take the next song in the audios 
+		 		$scope.audio = ngAudio.load(VariableFactory.audios[VariableFactory.currentSong +1]);
+		 		VariableFactory.currentSong = VariableFactory.currentSong +1;
+		 		$log.info("next audio:",$scope.audio);
+        	}
+
+        	// reload the view
+        	$state.reload();
+	 	}
+
+});
--- scripts/controllers/SearchController.js
+++ scripts/controllers/SearchController.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var app = angular.module('SentinaMoments')
- app.controller('SearchController',function($scope, $http, $log,$window){
+ app.controller('SearchController',function($scope, $http, $log, VariableFactory){
  	$scope.apiurl = "http://localhost:8080/services/"
+  $scope.user = {};
+  $scope.nonces = [];
  	$scope.searchResults = [];
  	$scope.currentResultPage = 0;
  	$scope.resultPageSize = 4; 	
@@ -10,114 +12,150 @@ var app = angular.module('SentinaMoments')
  	// Function to calculate how many pages are made from the search results
  	// Math.ceil rounds the result
  	$scope.numberOfPages = function() {
-        return Math.ceil($scope.searchResults.length/$scope.resultPageSize);                
+    return Math.ceil($scope.searchResults.length/$scope.resultPageSize);                
 	}
 	$scope.currentPage = function() {
 		return Math.ceil($scope.currentResultPage + 1);
 	}
 
-	$scope.getFromServer = function() {
+	$scope.searchRecipes = function() {
 
-	   	$http({
-	        method: 'GET',
-	        url: $scope.apiurl.concat('data/recipes'),
-	        headers: {
-	        'Accept': 'application/json'
-	   		},
-	   		params: {
-	   			contentQuery: $scope.searchQuery
-	   		}
+   	$http({
+        method: 'GET',
+        url: $scope.apiurl.concat('data/recipes'),
+        headers: {
+        'Accept': 'application/json'
+   		},
+   		params: {
+        start: 0,
+        hideDeleted: true,
+   			contentQuery: $scope.searchQuery
+   		}
 
-	    }).then(function successCallback(response) {
-	    	$scope.searchResults = [];
-	    	$scope.currentResultPage = 0;
+    }).then(function successCallback(response) {
+    	$scope.searchResults = [];
+    	$scope.currentResultPage = 0;
 
-	        $log.info("Success:", response.data);
+      $log.info("Success:", response.data);
 
-	        for (var i = 0; i < response.data.result.length; i++) {
-	        	$scope.searchResults[i] = response.data.result[i];
-	        }
+      $scope.searchResults = response.data.result;
 
+    }, function errorCallback(response) {
+        $log.error("ERROR:", response.data);
+    });
+  }
 
-	    }, function errorCallback(response) {
-	        $log.error("ERROR:", response.data);
-	    });
-   }
+  $scope.searchTracks = function() {
 
-   $scope.getNonce = function() {
-
-	   	$http({
-	        method: 'GET',
-	        url: $scope.apiurl.concat('data/audiofilenonces'),
-	        headers: {
-	        'Accept': 'application/json'
-	   		},
-	   		params: {
-	   			 sessionToken: ""
-	   		}
-
-	    }).then(function successCallback(response) {
-	        $log.info("Success:", response.data);
-
-
-	    }, function errorCallback(response) {
-	        $log.error("ERROR:", response.data);
-	    });
-   }
-
-	//Request to handle post/gets to the backend
-   $scope.request = function(method, route, headersObj, body) {
-      let headers = new Headers();
-      if (typeof headersObj === 'object') {
-        Object.keys(headersObj).forEach(function (key) {
-          let value = headersObj[key];
-          headers.append(key, value);
-        });
+    $http({
+        method: 'GET',
+        url: $scope.apiurl.concat('data/musicpieces'),
+        headers: {
+        'Accept': 'application/json'
+      },
+      params: {
+        start: 0,
+        hideDeleted: true,
+        similarityQuery: $scope.searchQuery
       }
-      headers.append('Accept', 'application/json');
-      return fetch('/services/' + route, {
-        method,
-        body,
-        headers,
-        credentials: 'same-origin'
-      }).then(resp => {
-        if (!resp.ok) {
-          if (resp.status >= 500 || resp.status < 400) {
-            throw new Error('Internal error: ' + resp.status);
-          }
-          return resp.json().then(json => {
-            console.error(json);
-            let err = new Error('Request failed');
-            throw err;
-          });
+
+    }).then(function successCallback(response) {
+      $scope.searchResults = [];
+      $scope.currentResultPage = 0;
+
+      $log.info("Success:", response.data);
+
+      $scope.searchResults = response.data.result;
+
+    }, function errorCallback(response) {
+        $log.error("ERROR:", response.data);
+    });
+  } 
+
+  $scope.searchPrograms = function() {
+
+    $http({
+        method: 'GET',
+        url: $scope.apiurl.concat('data/audioprograms'),
+        headers: {
+        'Accept': 'application/json'
+      },
+      params: {
+        start: 0,
+        hideDeleted: true,
+        similarityQuery: $scope.searchQuery
+      }
+
+    }).then(function successCallback(response) {
+      $scope.searchResults = [];
+      $scope.currentResultPage = 0;
+
+      $log.info("Success:", response.data);
+
+      $scope.searchResults = response.data.result;
+
+    }, function errorCallback(response) {
+        $log.error("ERROR:", response.data);
+    });
+  }
+
+  $scope.searchCategories = function() {
+
+    $http({
+        method: 'GET',
+        url: $scope.apiurl.concat('data/musiccategories'),
+        headers: {
+        'Accept': 'application/json'
+      },
+      params: {
+        start: 0,
+        hideDeleted: true,
+        contentQuery: $scope.searchQuery
+      }
+
+    }).then(function successCallback(response) {
+      $scope.searchResults = [];
+      $scope.currentResultPage = 0;
+
+      $log.info("Success:", response.data);
+
+      $scope.searchResults = response.data.result;
+
+    }, function errorCallback(response) {
+        $log.error("ERROR:", response.data);
+    });
+  }
+
+  $scope.audioTest = function() {
+    $scope.afId = 21983;
+    $scope.nonce = VariableFactory.nonces[0].nonce;
+
+    $http({
+          method: 'GET',
+          url: $scope.apiurl.concat('audiofile/' + $scope.afId + '/' + $scope.nonce + '/file.mp3'),
+          headers: {
+          'Accept': 'application/json'
         }
-         $log.info(route + ":", resp);
-         $log.info(document.cookie);
-        return resp.json();
+
+      }).then(function successCallback(response) {
+          $log.info("Success:", response);
+
+          $scope.audios = [];
+          $scope.audios[0] = $scope.apiurl.concat('audiofile/' + $scope.afId + '/' + $scope.nonce + '/file.mp3');
+          $log.info($scope.audios);
+
+      }, function errorCallback(response) {
+          $log.error("ERROR:", response.data);
       });
+
   };
-      
-    //Log in function
-    $scope.formPost = function(route) {
-        var _username = 'Metropolia1';
-        var _password = 'metr0609!'; 
-        
-        var cred = new FormData();
 
-        cred.append('username', _username);
-        cred.append('password', _password);
-      return $scope.request('POST', route, {}, cred);
-    }
+});
 
-   // temporary setup for authorizing the user
-   // TODO: Authorization through login page
-   $scope.formPost("auth/page/hashdb/login");
- });
-
- // Custom filter for the search results 
- app.filter('startFrom', function() {
+// Custom filter for the search results 
+app.filter('startFrom', function() {
     return function(input, start) {
         start = +start; //parse to int
         return input.slice(start);
     }
- });
+});
