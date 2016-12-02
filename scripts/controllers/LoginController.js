b50792af5d1361e47c882ae5d1621c7a030301c6
Oliver <oliver.cj.gill@gmail.com>
2016-12-02 10:44:53 +0200

    Session storage

--- scripts/controllers/LoginController.js
+++ scripts/controllers/LoginController.js
@@ -3,9 +3,37 @@
 var app = angular.module('SentinaMoments')
  app.controller('LoginController',function($scope, $http, $log, VariableFactory, RequestService, $state){
      
+     $scope.sessionCheck = function () {
+         $log.info("SessionCheck Called");
+      if (localStorage.username != null) {
+         $log.info("Session Check True");
+         var sessionuser = localStorage.username;
+         var sessionpass = localStorage.password;
+         
+         RequestService.loginFormPost("auth/page/hashdb/login", sessionuser, sessionpass);
+         RequestService.getUser();
+         
+         setTimeout(function(){
+            if (VariableFactory.user.id != null){   
+                $state.go('player');
+            }
+         }, 1000)
+         
+        }   
+     }
+     
      $scope.submit = function() {
          var _username = $('#username').val();
          var _password = $('#password').val();
+         
+         if (typeof(Storage) !== "undefined") {
+                localStorage.setItem("username", _username);
+                localStorage.setItem("password", _password);
+             $log.info("Session Set");
+        } else {
+            // Sorry! No Web Storage support..
+            $log.info("Sorry");
+        }
          
          RequestService.loginFormPost("auth/page/hashdb/login", _username, _password);
          RequestService.getUser();
@@ -16,4 +44,6 @@ var app = angular.module('SentinaMoments')
             }
          }, 1000)
      }
+     
+     $scope.sessionCheck();
  });
